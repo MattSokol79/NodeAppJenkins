f9e14fe90b6cc2b1fd00c5a52a2982384a040f13
Matt Sokol <maciej.sokol79@gmail.com>
2020-12-01 11:51:40 +0000

    Posts page updated with DB_HOST

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -6,6 +6,10 @@ sudo apt-get update -y
 # install git
 sudo apt-get install git -y
 
+# DB HOST write to the .bashrc our envrionment variable
+echo 'export DB_HOST=192.168.10.200' >> ~/.bashrc
+export DB_HOST=192.168.10.200
+
 # install nodejs
 sudo apt-get install python-software-properties -y
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
@@ -22,9 +26,6 @@ sudo rm /etc/nginx/sites-available/default
 sudo cp /home/ubuntu/environment/nginx.default /etc/nginx/sites-available/default
 
 sudo systemctl restart nginx.service
-
-# DB HOST write to the .bashrc our envrionment variable
-echo 'export DB_HOST=192.168.10.200' >> ~/.bashrc
 
 cd /home/ubuntu/app
 
